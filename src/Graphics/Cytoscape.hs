6364b5df806ca96592b1d499b0944446b24d4665
Tom Nielsen <tanielsen@gmail.com>
2017-07-06 17:50:22 +0100

    cdn
    Signed-off-by: Tom Nielsen <tanielsen@gmail.com>

---
+++ src/Graphics/Cytoscape.hs
@@ -0,0 +1,8 @@
+module Graphics.Cytoscape where
+
+import Data.Aeson
+import Lucid
+
+
+cytoscapeCDN :: Monad m => HtmlT m ()
+cytoscapeCDN = script_ [src_ "https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.1.2/cytoscape.min.js"] ""
