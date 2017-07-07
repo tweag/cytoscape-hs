a3b8f25f1d89e0b0d3e40c96f452d530733b0a7d
Tom Nielsen <tanielsen@gmail.com>
2017-07-07 11:51:04 +0100

    Initial bindings

--- TestCytoscape.hs
+++ TestCytoscape.hs
@@ -3,6 +3,7 @@
 import qualified Data.Text.Lazy as T
 import qualified Data.Text.Lazy.IO as T
 import Lucid
+import Lucid.Bootstrap3
 import Graphics.Cytoscape
 
 main :: IO ()
@@ -12,7 +13,18 @@ main = do
 testpage =  doctypehtml_ $ do
   head_ $ do
     meta_ [charset_ "utf-8"]
+    cdnJqueryJS
+    cdnBootstrapJS
     cytoscapeCDN
 
   body_ $ do
      "Hello World"
+     div_ [id_ "cy"] ""
+     script_ $ runCytoscape nodes edges (Layout "cose") styles "#cy"
+     style_ "#cy { height: 600px; width: 600px}"
+
+nodes = [Node $ NodeData "a" "foo", Node $ NodeData "b" "bar"]
+
+edges = [Edge $ EdgeData "a1" "a" "b"]
+
+styles = [Style "node" [("color","black"),("content","data(name)")], Style "edge" []]
